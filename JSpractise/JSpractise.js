720217ab22bdbc0633c99cc1b0aea2c8aecbb56c
Jayashree <s.jayashree94@gmail.com>
2025-06-30 04:52:18 +0530

    day 5

--- JSpractise/JSpractise.js
+++ JSpractise/JSpractise.js
@@ -1,7 +1,15 @@
-var b = function(param){
-    return function xoz(){
+console.log("start");
+setTimeout(function cb(){
+    console.log("Call back");
 
-}
-}
+},5000);
 
-console.log(b());
+let startDate = new Date().getTime();
+endDate = startDate;
+while(endDate<startDate + 10000){
+console.log("start date "+ startDate);
+
+endDate = new Date().getTime();
+console.log("enddate: " + endDate) 
+}
+console.log('while goes out"')
