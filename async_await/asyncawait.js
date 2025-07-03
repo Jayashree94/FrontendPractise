7392f2de470b54888e876fe49a00d5319bd02b99
Jayashree <s.jayashree94@gmail.com>
2025-07-03 06:06:05 +0530

    asyncawait

--- async_await/asyncawait.js
+++ async_await/asyncawait.js
@@ -4,8 +4,15 @@ resolve("promise returned");
 
 async function getdata(){
     //always return a promise
+    const val = await p;
+    //only used inside async
+}
+
+////////////////////////////////////////////
+
+function getdata1(){
     return p;
 }
 
-const dataPromise = getdata();
-getdata().then(res=>console.log(res));
+const dataPromise2 = getdata1();
+dataPromise2().then(res=>console.log(res));
