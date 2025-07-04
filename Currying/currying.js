89072e5bf1d97325da191c4c81b1ce0ef149e353
Jayashree <s.jayashree94@gmail.com>
2025-07-04 06:49:11 +0530

    currying

--- Currying/currying.js
+++ Currying/currying.js
@@ -1,6 +1,15 @@
-let multiply = function (x,y){
-    console.log(x*y);
+//let multiply = function (x,y){
+//    console.log(x*y);
+//}
+
+//let multiplybytwo = multiply.bind(this,2);
+//multiplybytwo(4);
+
+let multiply = function(x){
+    return function(y){
+        console.log(x*y);
+    }
 }
 
-let multiplybytwo = multiply.bind(this,2);
-multiplybytwo(4);
+const curriedmultiply = multiply(2);
+curriedmultiply(7);
